3cea347f550fb352de3516eca4375283d594cc94
cedricfonsat <fonsat.pro@gmail.com>
2024-06-20 11:31:55 +0200

    fix responsive and add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,6 +46,18 @@ module.exports = {
         'position': 'left, bottom',
         'width': 'width',
       },
+        // that is animation class
+        animation: {
+          fade: 'fadeOut 5s ease-in-out',
+        },
+  
+        // that is actual animation
+        keyframes: theme => ({
+          fadeOut: {
+            '0%': { backgroundColor: theme('colors.red.300') },
+            '100%': { backgroundColor: theme('colors.transparent') },
+          },
+        }),
     },
   },
   plugins: [
