f9795862bb82fb72a9af70efdaecc09ac2856940
Lucas-HAFO <lucas@hafowebsolutions.fr>
2024-06-21 19:31:46 +0200

    Header adjustment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,73 +1,74 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-  ],
+  content: ["./assets/**/*.js", "./templates/**/*.html.twig"],
   theme: {
     extend: {
       colors: {
-        primary: '#171721',
-        secondary: '#00A3FF',
-        tertiary: '#212E48',
-        quaternary: '#191925',
-        quinternary: '#1B1B28',
-        grayText: '#ACACAC',
-        whiteTransparency: 'rgba(255, 255, 255, 0.00784)',
-        borderBase: '#282832',
-        dark: '#1D1D1D',
-        customBlack: 'rgba(0, 0, 0, 0.8)',
-        darkBlue: '#242435',
+        primary: "#171721",
+        secondary: "#00A3FF",
+        tertiary: "#212E48",
+        quaternary: "#191925",
+        quinternary: "#1B1B28",
+        grayText: "#ACACAC",
+        whiteTransparency: "rgba(255, 255, 255, 0.00784)",
+        borderBase: "#282832",
+        dark: "#1D1D1D",
+        customBlack: "rgba(0, 0, 0, 0.8)",
+        darkBlue: "#242435",
+        darkMidnightBlue: "#151521",
       },
       boxShadow: {
-        '3xl': '0 0 15px rgba(0,0,0,.15)',
+        "3xl": "0 0 15px rgba(0,0,0,.15)",
       },
       padding: {
-        paddingBase: '12.166%',
-        paddingDemi: '6.083%',
-        100: '100px',
-        50: '50px',
+        paddingBase: "12.166%",
+        paddingDemi: "6.083%",
+        100: "100px",
+        50: "50px",
       },
       margin: {
-        marginBase: '12.166%',
-        marginDemi: '6.083%',
-        100: '100px',
-        50: '50px',
+        marginBase: "12.166%",
+        marginDemi: "6.083%",
+        100: "100px",
+        50: "50px",
+      },
+      fontSize: {
+        '15': '15px',
       },
       screens: {
         // Navigation breakpoints
-        'nav-lg': { min: '1158px', max: '1344px' },
-        'nav-sm': { min: '769px', max: '1157px' },
-        'nav-min': { min: '0px', max: '639px' },
-        'lg-1500': '1400px',
+        "nav-lg": { min: "1158px", max: "1344px" },
+        "nav-sm": { min: "769px", max: "1157px" },
+        "nav-min": { min: "0px", max: "639px" },
+        "lg-1500": "1400px",
       },
       transitionProperty: {
-        'position': 'left, bottom',
-        'width': 'width',
+        position: "left, bottom",
+        width: "width",
       },
     },
   },
   plugins: [
     function ({ addUtilities }) {
       const newUtilities = {
-        '@media (max-height: 546px)': {
-          '.hide-on-small-height': {
-            display: 'none',
+        "@media (max-height: 723px)": {
+          ".static-on-small-screen": {
+            position: "static",
           },
         },
-      }
+      };
 
-      addUtilities(newUtilities)
+      addUtilities(newUtilities);
     },
     function ({ addComponents }) {
       const newComponents = {
-        '.mobile-container': {
-          '@apply w-full mx-auto px-4 py-0.5': {},
+        ".default-container": {
+          "@apply w-full mx-auto px-8 py-0.5": {},
         },
-      }
+      };
 
-      addComponents(newComponents)
+      addComponents(newComponents);
     },
   ],
-}
+};
