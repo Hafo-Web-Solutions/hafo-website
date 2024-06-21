c000ddd61da6463daba46366a98bc9462777ce40
Lucas-HAFO <lucas@hafowebsolutions.fr>
2024-06-21 21:15:08 +0200

    Back to top

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,7 @@ import './styles/app.css';
 import sal from 'sal.js'
 import 'sal.js/dist/sal.css';
 
+// SAL initialization
 document.addEventListener('DOMContentLoaded', () => {
   sal({
     threshold: 0.5,
@@ -9,3 +10,41 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+// Back to top
+document.addEventListener('DOMContentLoaded', function() {
+    "use strict";
+
+    var progressPath = document.querySelector('.rn-progress-parent path');
+    var pathLength = progressPath.getTotalLength();
+    progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+    progressPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+    progressPath.style.strokeDashoffset = pathLength;
+    progressPath.getBoundingClientRect();
+    progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+
+    var updateProgress = function() {
+        var scroll = window.pageYOffset || document.documentElement.scrollTop;
+        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var progress = pathLength - (scroll * pathLength / height);
+        progressPath.style.strokeDashoffset = progress;
+    };
+
+    updateProgress();
+    window.addEventListener('scroll', updateProgress);
+
+    var offset = 50;
+
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset > offset) {
+            document.querySelector('.rn-progress-parent').classList.add('rn-backto-top-active');
+        } else {
+            document.querySelector('.rn-progress-parent').classList.remove('rn-backto-top-active');
+        }
+    });
+
+    document.querySelector('.rn-progress-parent').addEventListener('click', function(event) {
+        event.preventDefault();
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        return false;
+    });
+});
