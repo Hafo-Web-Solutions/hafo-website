bc5287496375e78ada4d8d2cbeb748db526c0975
Lucas-HAFO <lucas@hafowebsolutions.fr>
2024-06-21 20:17:01 +0200

    SAL

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,11 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import sal from 'sal.js'
+import 'sal.js/dist/sal.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  sal({
+    threshold: 0.5,
+    once: true,
+  });
+});
+
