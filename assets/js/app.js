dffd7ec75ef4690894a0170555e27677f7796c24
Lucas-HAFO <lucas@hafowebsolutions.fr>
2024-06-25 19:19:30 +0200

    Blog component

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
 import "../css/app.css";
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 import sal from "sal.js";
 import "sal.js/dist/sal.css";
+import Swiper from "swiper";
+import "swiper/swiper-bundle.css";
 
 // Dynamic javaScript import from vendor directory
-const requireContext = require.context('./vendor', true, /\.js$/);
+const requireContext = require.context("./vendor", true, /\.js$/);
 requireContext.keys().forEach(requireContext);
 
 // SAL initialization
@@ -16,6 +18,40 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // AlpineJS initialization
-window.Alpine = Alpine;
-Alpine.start();
+document.addEventListener("DOMContentLoaded", () => {
+  window.Alpine = Alpine;
+  Alpine.start();
+});
 
+// Swiper initialization & blog slider
+document.addEventListener("DOMContentLoaded", () => {
+  const swiperInstance = new Swiper(".swiper-container", {
+    direction: "horizontal",
+    spaceBetween: 32,
+    loop: true,
+    speed: 600,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+      nextEl: "#next",
+      prevEl: "#prev",
+    },
+    touchRatio: 1,
+    slidesPerView: 1,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+        loop: false,
+      },
+      1280: {
+        slidesPerView: 4,
+        loop: false,
+      },
+    },
+  });
+});
